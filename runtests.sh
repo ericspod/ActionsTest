40bcfdcb127ba0940ece257457d5a845050adbee
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2026-02-27 18:31:20 -0500

    Adding Precommit Hook files (#6)
    * Adding Precommit Hook files
    
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update
    
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    
    * Oops
    
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    
    * Dependency
    
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    
    * Use python but run with bash
    
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ runtests.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+PY_EXE=${TEST_PY_EXE:-$(which python)}
+
+homedir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Run tests under $homedir"
+cd "$homedir"
+
+echo "Using Python '$PY_EXE'"
+
+function is_pip_installed() {
+	return $("${PY_EXE}" -c "import sys, importlib.util; sys.exit(0 if importlib.util.find_spec(sys.argv[1]) else 1)" $1)
+}
+
+if ! is_pip_installed black
+then
+    pip install black
+fi
+
+"${PY_EXE}" -m black -l 120 --skip-magic-trailing-comma "$homedir"
--- test_module/components.py
+++ test_module/components.py
@@ -1,20 +1,19 @@
-
 from math import sqrt
 
 
-def func(x:int=0) -> str:
+def func(x: int = 0) -> str:
     """
     This function returns "zero" if x is 0, else the string version of x.
     """
-    if x==0:
+    if x == 0:
         # return "zero" for demo purposes
         return "zero"
     else:
         return str(x)
 
 
-def abs(value:int|float):
+def abs(value: int | float):
     """
     Return the absolute value of `value`.
     """
-    return sqrt(value*value)
+    return sqrt(value * value)
