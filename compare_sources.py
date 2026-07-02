ff2ee310c30f2fd97c2532591465385470f88f20
Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
2026-07-01 22:31:07 -0230

    New Action Skip Format (#18)
    * New Action Skip Format
    
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    
    * [pre-commit.ci lite] apply automatic fixes
    
    ---------
    
    Signed-off-by: Eric Kerfoot <17726042+ericspod@users.noreply.github.com>
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- compare_sources.py
+++ compare_sources.py
@@ -24,8 +24,8 @@ def sources_equal(src1: str, src2: str) -> bool:
     """
     Compare Python source files at the AST level without docstrings or comments. If two files are equal except for
     changes to docstrings or comments, they will be considered equal by this function. Any other changes will appear
-    as differences in the AST representations, which are compared innefficiently by dumping parts of the tree to string
-    representations and compared
+    as differences in the AST representations, which are compared inefficiently by dumping parts of the tree to string
+    representations and compared.
     """
     remdoc = RemoveDocstrings()
 
@@ -38,14 +38,8 @@ def sources_equal(src1: str, src2: str) -> bool:
     if len(list1) != len(list2):
         return False
 
-    for n1, n2 in zip(list1, list2):
-        if type(n1) is not type(n2):
-            return False
-
-        if ast.dump(n1) != ast.dump(n2):  # TODO: more efficient way of doing this?
-            return False
-
-    return True
+    # TODO: more efficient way than using ast.dump?
+    return all(type(n1) is type(n2) and ast.dump(n1) == ast.dump(n2) for n1, n2 in zip(list1, list2))
 
 
 def files_considered_equal(file1: str, file2: str) -> bool:
@@ -55,19 +49,21 @@ def files_considered_equal(file1: str, file2: str) -> bool:
     _, ext1 = splitext(file1)
     _, ext2 = splitext(file2)
 
-    if ext1 != ext2:  # if extensions aren't equal then definitely different
+    # if extensions aren't equal then definitely different (which shouldn't happen anyway)
+    if ext1 != ext2:
         return False
-    elif ext1 in SKIP_EXTS:  # if extensions are an ignored type, ie. docs, don't compare at all
+
+    # if extensions are an ignored type, ie. docs, don't compare at all
+    if ext1 in SKIP_EXTS:
         return True
-    elif ext1 != ".py":  # if not Python source files, assume different
+
+    # if not Python source files, they aren't doc files at this point so assume different
+    if ext1 != ".py":
         return False
 
     # compare the actual parsed contents of the source files
     with open(file1) as o1, open(file2) as o2:
-        if not sources_equal(o1.read(), o2.read()):
-            return False
-
-    return True
+        return sources_equal(o1.read(), o2.read())
 
 
 if __name__ == "__main__":
